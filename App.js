6d79798b65f2eef03e29ba5ff07f61c481fe1cb3
Orkun Durmaz <orkundurmaz90@gmail.com>
2022-01-23 10:12:20 +0300

    React Native class #1

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  //useState methodu ilk elemani degisken, ikinci elemani method olan bir array
+  //donduruyor. Bu arrayi destructure ederek kullaniyoruz. Destructure ettigimiz
+  //yapi bir getter/setter
+  const [text, setText] = useState("Hello World!");
+  const [textInput, setTextInput] = useState("");
+
+  //Button'a basildiginda tetiklenen fonksiyon
+  const handleOnPress = () => {
+    let _text = textInput.trim();
+    if (_text && _text.length >= 3) {
+      _text = _text[0].toUpperCase() + _text.slice(1)
+      setText(_text)
+    }
+    else {
+      alert("Metin 3 harften kisa olamaz!")
+    }
+    setTextInput("")
+  }
+
+  //TextInput uzerindeki metin degistiginde tetiklenen fonskiyon
+  const handleOnChangeText = (text) => setTextInput(text);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}>
+        <Text style={styles.header}>Welcome to the first class of React Native!</Text>
+        <Text style={styles.text}>{text}</Text>
+        <TextInput value={textInput} onChangeText={handleOnChangeText} style={styles.textInput} placeholder='Write here...' />
+        <Button title={"Press on me!"} onPress={handleOnPress} />
+      </View>
     </View>
   );
 }
@@ -13,8 +40,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'skyblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box: {
+    height: 250,
+    justifyContent: "space-around",
+    alignItems: 'center',
+    borderWidth: 5,
+    padding: 5,
+    borderRadius: 10,
+    borderColor: 'green'
+  },
+  header: {
+    fontSize: 16,
+    color: 'white',
+    fontWeight: 800,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 800,
+  },
+  textInput: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 5,
+    paddingVertical: 3,
+    borderRadius: 5
+  }
 });
